666611a790dd501caab44af953d1a35e18a18309
John Dale <johnkdale02@gmail.com>
2023-06-27 22:21:15 -0400

    Updated Functions

--- Morning-Email-MP3.py
+++ Morning-Email-MP3.py
@@ -15,15 +15,15 @@ from datetime import datetime
 
 SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
     # Will have the user Authenticate themselves through the Gmail GoogleAPI
-openai.api_key = 'ENTER YOUR OPENAI API_KEY HERE'
+openai.api_key = 'YOUR_OPEN_AI_KEY_HERE'
     # Key for integrating ChatGPT for summarization purposes
-savePath = "C:/Users/ENTER_YOUR_USER/Dropbox/Public/Emaildebrief.mp3"
+savePath = "C:/Users/YOUR USER/Dropbox/Public/Emaildebrief.mp3"
     # Location where the final MP3 should be stored ---> Put into Public Dropbox Folder!
-TwilioAccountSid  = 'TWILIO_ACCOUNT_SID' 
-TwilioAuthToken = 'TWILIO_AUTH_TOKEN'
-myTwilioNumber = '+YOUR_TWILIO_NUMBER'
-myCellPhone = '+PHONE_NUMBER_TO_RECIEVE_DEBRIEF'
-MP3media = 'https://www.dropbox.com/URL_THAT_LEADS_TO_PUBLIC_DROPBOX_FILE_STORED_IN_SAVE_PATH_ABOVE'
+TwilioAccountSid  = 'YOUR_PERSONAL_TWILIO_SID' 
+TwilioAuthToken = 'YOUR_PERSONAL_TWILIO_AUTH_TOKEN'
+myTwilioNumber = '+YOUR_PERSONAL_TWILIO_NUMBER'
+myCellPhone = '+YOUR_PERSONAL_CELL_PHONE_NUMBER'
+MP3media = 'https://www.dropbox.com//home/ADD_IN_YOUR_PATH_TO_THE_EMAIL_FILE'
     # Twilio authentication SID and Token & Number to recieve Debrief messages
 
 #--------------------- Personalization: -------------------------------------------------
@@ -38,7 +38,7 @@ monthTranslation = {'Jan': ['January', '1'],
                     'March': ['March', '3'],
                     'April': ['April', '4'],
                     'May': ['May', '5'],
-                    'June': ['June', '6'],
+                    'Jun': ['June', '6'],
                     'July': ['July', '7'],
                     'Aug': ['August', '8'],
                     'Sept': ['September', '9'],
@@ -50,7 +50,7 @@ monthTranslation = {'Jan': ['January', '1'],
 weekdayTranslations = {'Mon,': 'Monday',
                        'Tue,' : 'Tuesday',
                        'Wed,': 'Wednesday',
-                       'Thur,': 'Thursday',
+                       'Thu,': 'Thursday',
                        'Fri,': 'Friday',
                        'Sat,': 'Saturday',
                        'Sun,': 'Sunday',
@@ -128,10 +128,8 @@ def isTooOld(distance, hoursOffline, messageCount, messagesAllowed):
         return True
     else:
         return False
-
-def getEmails():
-    # Variable creds will store the user access token.
-    # If no valid token found, we will create one.
+    
+def verifyCredentials():
     creds = None
     tooOld = False  # Emails are not yet too old
 
@@ -148,143 +146,167 @@ def getEmails():
         # Save the credentials for the next run
         with open('token.json', 'w') as token:
             token.write(creds.to_json())
-    
+        
+    return creds
+
+def chatGptSummary(body, subject, sender):
+
+    Summary = ''  # create a empty string for this message summary
+    Prompt = "Summarize this email into a single sentence: "
+
+    if body == None or body == '':
+        chatGPTinput = Prompt + "Email Subject: " + subject + " Email Sender: " + sender 
+    else:
+        chatGPTinput = Prompt + "Email Subject: " + subject + " Email Sender: " + sender + " Email body " + body
+        if len(chatGPTinput) > 4000:
+            chatGPTinput = chatGPTinput[:4000]
+
+    try:
+        chatGPToutput = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=chatGPTinput,
+            temperature=0.7,
+            max_tokens=709,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0,
+            )
+        
+        for i in chatGPToutput.choices:
+            Summary += i.text
+
+    except Exception as e:
+        print(f"\nSome unread emails still await reading: Error {e}")
+        Summary+= "Some unread emails still await reading"
+
+    return Summary
+
+def connectToMessages(creds):
     try:
         # Connect to the Gmail API
         service = build('gmail', 'v1', credentials=creds)
-    
+
         # request a list of all the messages
         #result = service.users().messages().list(userId='me').execute()
-    
+
         # We can also pass maxResults to get any number of emails. Like this:
         result = service.users().messages().list(maxResults=messagesAllowed, labelIds = ["INBOX", "UNREAD"], userId='me').execute()
         messages = result.get('messages', [])
-    
+
         # messages is a list of dictionaries where each dictionary contains a message id.
-    
-        AllEmailSummaries = ""  # variable to hold all email summary text
-        
-        messageCount = 0
-        distance = 0
-
-        # iterate through all the messages
-        for msg in messages:
-
-            # Get the message from its id
-            txt = service.users().messages().get(userId='me', id=msg['id']).execute()
-            
-            Summary = "" # empty string to hold each summary
-
-            # Use try-except to avoid any Errors
-            try:
-                # Get value of 'payload' from dictionary 'txt'
-                payload = txt['payload']
-            
-                headers = payload['headers']
-    
-                # Look for Subject and Sender Email in the headers
-                for values in headers:
-                    if values['name'] == 'Subject':
-                        subject = values['value']
-                    if values['name'] == 'From':
-                        sender = values['value']
-                    if values['name'] == 'Date':
-                        dateRaw = values['value']
-                    
-
-                    def emailDate(dateRaw):
-                        hour = None # making sure hourRaw is chaned w/correct time zone
-                        MN = "AM"   # by default times are AM
-
-                        dateSplit = dateRaw.split()  # splits raw date data into weekday, day number, month, year
-
-                        weekday = weekdayTranslations[dateSplit[0]]  # gets Monday, Tuesday... 
-                        dayRaw = dateSplit[1]   # get the day number of month 1, 10, 19 ...
-                        day = dateTranslation[dayRaw]   # gets 1st, 2nd, 3rd .....
-                        month = monthTranslation[dateSplit[2]][0]  # gets month January, March, April .... 
-                        monthRaw = monthTranslation[dateSplit[2]][1] # get month number 1, 6 .... 12 
-                        year = dateSplit[3]  # gets year 2023, 2019....
-                        timezone = dateSplit[5]  # gets '+0000' [timezone]
-                        
-                        timeRaw = dateSplit[4]   # gets 19:54:26 -> break into hours, minutes
-                        hourRaw = timeRaw[0:2]  # hours
-                        minute = timeRaw[3:5]  # minutes
-                        
-                            # correcting for timezones
-                        if timezone == '+0000':   
-                            hour = str((int(hourRaw) + 19) % 24)
-                        if timezone == '-0600':
-                            hour = str((int(hourRaw) + 1) % 24)
-
-                            # military to normal [AM or PM]
-                        if hour != None and int(hour) >= 12: 
-                            hour = str(int(hour) - 12)
-                            MN = "PM" 
-                        
-                        time = hour + ":" + minute + " " + MN
-                        date = weekday + " " +  month + " " +  day + " at " + time
-                        distance = getDistance(int(hour), int(dayRaw), int(monthRaw), int(year))
-
-                        return distance
-                
-                distance = emailDate(dateRaw)
-
-                tooOld = isTooOld(distance, hoursOffline, messageCount, messagesAllowed)
-
-                if tooOld == True:
-                    return AllEmailSummaries, messageCount
-
-                # The Body of the message is in Encrypted format. So, we have to decode it.
-                # Get the data and decode it with base 64 decoder.
-                try:
-                    parts = payload.get('parts')[0]
-                    data = parts['body']['data']
-                    data = data.replace("-","+").replace("_","/")
-                    decoded_data = base64.b64decode(data)
-                    soup = BeautifulSoup(decoded_data , "lxml")
-                    body = soup.body.string   # prints body of text as string
-                except:
-                    body = ''
-                    pass
-            except:
-                print("Cannot get payload and headers")
-            
-            # Creating prompt for ChatGPT and creating each summary text to be entered
-
-            Prompt = "Summarize this email into a single sentence: "
-
-            if body == None or body == '':
-                chatGPTinput = Prompt + "Email Subject: " + subject + " Email Sender: " + sender 
-            else:
-                chatGPTinput = Prompt + "Email Subject: " + subject + " Email Sender: " + sender + " Email body " + body
-
-            try:
-                chatGPToutput = openai.Completion.create(
-                    engine="text-davinci-003",
-                    prompt=chatGPTinput,
-                    temperature=0.7,
-                    max_tokens=709,
-                    top_p=1,
-                    frequency_penalty=0,
-                    presence_penalty=0,
-                    )
-                
-                for i in chatGPToutput.choices:
-                    Summary += i.text
-
-            except:
-                print("\nSome Unread Emails still await you, ChatGPT has trouble summarizing them")
-
-            # Creating block of text == All email Summaries
-            messageCount += 1
-            AllEmailSummaries += str(messageCount) + "). Recieved " + str(distance) + " hours ago." + Summary + "\n\n"
 
     except HttpError as error:
         # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
+        print(f'Unable to connect to Gmail Service: {error}')
+        exit()
+
+    return messages, service
+
+def readEmails(messages, service):
+
+    AllEmailSummaries = ""  # variable to hold all email summary text
+    messageCount = 0
+    distance = 0
     
+   # iterate through all the messages
+    for msg in messages:
+        
+        # Get the message from its id
+        txt = service.users().messages().get(userId='me', id=msg['id']).execute()
+
+        # Use try-except to avoid any Errors
+        
+        # Get value of 'payload' from dictionary 'txt'
+        payload = txt['payload']
+        headers = payload['headers']
+
+        # Look for Subject and Sender Email in the headers
+        for values in headers:
+            if values['name'] == 'Subject':
+                subject = values['value']
+            if values['name'] == 'From':
+                sender = values['value']
+            if values['name'] == 'Date':
+                dateRaw = values['value']
+            
+        distance = emailDate(dateRaw)
+        tooOld = isTooOld(distance, hoursOffline, messageCount, messagesAllowed)
+
+        if tooOld == True:
+            return AllEmailSummaries, messageCount
+
+        # decode the message details and form the email into a string
+        try:
+            parts = payload.get('parts')[0]  
+            data = parts['body']['data']
+            data = data.replace("-","+").replace("_","/")
+            decoded_data = base64.b64decode(data)
+            soup = BeautifulSoup(decoded_data , "lxml")
+            body = soup.body.string   # prints body of text as string
+
+        except Exception as e:
+            print(f"Couldn't get paylods, Error {e}")
+            body = f"Could not summarize email from {sender}, read it manually"
+            
+        # Creating prompt for ChatGPT and creating each summary text to be entered
+
+        Summary = chatGptSummary(body, subject, sender)
+
+        messageCount += 1
+            # Creating block of text == All email Summaries
+
+
+        AllEmailSummaries += str(messageCount) + "). Recieved " + str(distance) + " hours ago." + Summary + "\n\n"
+
     return AllEmailSummaries, messageCount
- 
+
+def emailDate(dateRaw):
+    hour = None # making sure hourRaw is chaned w/correct time zone
+    MN = "AM"   # by default times are AM
+
+    dateSplit = dateRaw.split()  # splits raw date data into weekday, day number, month, year
+
+    weekday = weekdayTranslations[dateSplit[0]]  # gets Monday, Tuesday... 
+    dayRaw = dateSplit[1]   # get the day number of month 1, 10, 19 ...
+    day = dateTranslation[dayRaw]   # gets 1st, 2nd, 3rd .....
+    month = monthTranslation[dateSplit[2]][0]  # gets month January, March, April .... 
+    monthRaw = monthTranslation[dateSplit[2]][1] # get month number 1, 6 .... 12 
+    year = dateSplit[3]  # gets year 2023, 2019....
+    timezone = dateSplit[5]  # gets '+0000' [timezone]
+    
+    timeRaw = dateSplit[4]   # gets 19:54:26 -> break into hours, minutes
+    hourRaw = timeRaw[0:2]  # hours
+    minute = timeRaw[3:5]  # minutes
+    
+        # correcting for timezones
+    print(timezone)
+    if timezone == '+0000' or timezone == 'GMT':
+        hour = str((int(hourRaw) + 19) % 24)
+    if timezone == '-0600':
+        hour = str((int(hourRaw) + 1) % 24)
+    if timezone == '-0700':
+        hour = str((int(hourRaw) + 2) % 24)
+    if timezone == '-0400':
+        hour = str((int(hourRaw) + 1) % 24)
+        
+        # military to normal [AM or PM]
+    if hour != None and int(hour) >= 12: 
+        hour = str(int(hour) - 12)
+        MN = "PM" 
+    
+    time = hour + ":" + minute + " " + MN
+    date = weekday + " " +  month + " " +  day + " at " + time
+    distance = getDistance(int(hour), int(dayRaw), int(monthRaw), int(year))
+
+    return distance
+
+def getEmails():
+    
+    creds = verifyCredentials()
+    messages, service = connectToMessages(creds)
+    AllEmailSummaries, messageCount = readEmails(messages, service)
+
+    return AllEmailSummaries, messageCount
 
 def twilioSendEmailSummary():
 
@@ -292,18 +314,24 @@ def twilioSendEmailSummary():
 
     MP3_Text = "Debrief of your " + str(messageCount) + " unread email(s) over the past " +  str(hoursOffline) + " hours.\n\n" +  AllEmailsString
 
-    print(MP3_Text)
+    print(f"MP3_Text: {MP3_Text}")
 
     EmailMP3 = gTTS(text=MP3_Text, lang='en', slow=False)  # Email imput, english, Not slow
     EmailMP3.save(savePath)
 
     twilioCli = Client(TwilioAccountSid, TwilioAuthToken)
-    
-    message = twilioCli.messages.create(
-    from_=myTwilioNumber,
-    to=myCellPhone,
-    body = MP3media,
-    )
-    
+    print(type(twilioCli))
 
-twilioSendEmailSummary()
+    message = twilioCli.messages.create(
+        body=f'Here is an MP3 of your {messageCount} unread emails over the past {hoursOffline} hours. >> {MP3media} <<',
+        from_=myTwilioNumber,
+        to=myCellPhone
+    )
+
+    return twilioCli, messageCount
+
+
+if __name__ == '__main__':
+    
+    twilioSendEmailSummary()
+   
